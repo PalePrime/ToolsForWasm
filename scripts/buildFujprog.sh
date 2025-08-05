98acc59c5f759d88966bfd224ff99b808670fdec
Per Andersson <per@palenetworks.com>
2025-08-05 20:12:12 +0200

    Fix scripts for libftdi and fujprog

--- scripts/buildFujprog.sh
+++ scripts/buildFujprog.sh
@@ -7,7 +7,15 @@ SRC_PATH=$SRC_ROOT/fujprog
 DST_PATH=$WASM_ROOT/fujprog
 
 
-#$EM_ROOT/emcmake cmake -S $SRC_PATH -B $BUILD_ROOT/buildFujprogWasm \
-#  -DCMAKE_EXE_LINKER_FLAGS="$BASE_EM_LDFLAGS"
+$EM_ROOT/emcmake cmake  --fresh -S $SRC_PATH -B $BUILD_ROOT/buildFujprogWasm \
+  -DLIBFTDI_INCLUDE_DIRS="$WASM_ROOT/libftdi/include/libftdi1" \
+  -DLIBFTDI_LIBRARIES="-L$WASM_ROOT/libftdi/lib -lftdi1" \
+  -DLIBUSB_INCLUDE_DIRS="$WASM_ROOT/libusb/include/libusb-1.0" \
+  -DLIBUSB_LIBRARIES="-L$WASM_ROOT/libusb/lib -lusb-1.0" \
+  -DCMAKE_EXE_LINKER_FLAGS="\
+    $BASE_EM_LDFLAGS \
+    -lembind \
+    -sASYNCIFY=1 \
+  "
 
-#cmake --build $BUILD_ROOT/buildFujprogWasm
+cmake --build $BUILD_ROOT/buildFujprogWasm
